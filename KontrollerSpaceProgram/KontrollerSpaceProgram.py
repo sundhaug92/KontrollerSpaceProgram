3f576108f49eee4c6b83daf3eea315351831adba
Martin Sundhaug <martinsundhaug@gmail.com>
2015-06-16 01:12:36 +0200

    Initial commit

---
+++ KontrollerSpaceProgram/KontrollerSpaceProgram.py
@@ -0,0 +1,77 @@
+import threading,krpc,math
+
+targetAltitude=100000
+preApoWait=10
+class AutoStager(threading.Thread):
+    def run(x):
+        print "AutoStager ON"
+        vessel=conn.space_center.active_vessel
+        while True:
+            res=vessel.resources_in_decouple_stage(vessel.control.current_stage,False)
+            if (res.amount("LiquidFuel")==0) and vessel.control.current_stage > 0:
+                vessel.control.activate_next_stage()
+
+class AutoMANU(threading.Thread):
+    def run(x):
+        print "AutoMANU ON"
+        vessel=conn.space_center.active_vessel
+        flight=vessel.flight(vessel.orbital_reference_frame)
+        while True:
+            ms=flight.mach*flight.speed_of_sound
+            pitch,roll,heading=90,0,90
+            if ms<1800 and flight.speed_of_sound>0:
+                pitch=90*math.cos(3*ms/1800)
+            else:
+                pitch=0
+            if pitch < 0:
+                pitch = 0
+            vessel.auto_pilot.set_rotation(pitch,heading,roll)
+class AutoMANO(threading.Thread):
+    def run(x):
+        print "AutoMANO ON"
+        vessel=conn.space_center.active_vessel
+        flight=vessel.flight(vessel.orbital_reference_frame)
+        while True:
+            ms=flight.mach*flight.speed_of_sound
+            maxThrottle=1
+            if flight.terminal_velocity < ms:
+                maxThrottle=flight.terminal_velocity/ms
+            if vessel.orbit.apoapsis_altitude<targetAltitude:
+                conn.space_center.active_vessel.control.throttle=maxThrottle
+            elif vessel.orbit.periapsis_altitude < targetAltitude and vessel.orbit.time_to_apoapsis > preApoWait:
+                conn.space_center.active_vessel.control.throttle=0
+            elif vessel.orbit.periapsis_altitude < targetAltitude:
+                conn.space_center.active_vessel.control.throttle=maxThrottle
+            else:
+                conn.space_center.active_vessel.control.throttle=0
+            pass
+
+print "Awaiting connection"
+conn = krpc.connect(name='Kontroller')
+
+mano=AutoMANO()
+print "Starting mano",
+mano.start()
+while not mano.isAlive():
+    print ".",
+print "OK"
+
+manu=AutoMANU()
+print "Starting manu",
+manu.start()
+while not manu.isAlive():
+    print ".",
+print "OK"
+
+stager=AutoStager()
+print "Starting stager",
+stager.start()
+while not stager.isAlive():
+    print ".",
+print "OK"
+
+print "All managers started"
+
+while True:
+    pass
+

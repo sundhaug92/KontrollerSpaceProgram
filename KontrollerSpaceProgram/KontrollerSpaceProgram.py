1972ef4a5aa955b8821af9db63de4d5142aa1f37
Martin Sundhaug <martinsundhaug@gmail.com>
2015-06-16 04:15:26 +0200

    manu and mano-modes, fixed T1vT2-cutoffs

--- KontrollerSpaceProgram/KontrollerSpaceProgram.py
+++ KontrollerSpaceProgram/KontrollerSpaceProgram.py
@@ -20,18 +20,25 @@ class AutoMANU(threading.Thread):
         while True:
             ms=flight.mach*flight.speed_of_sound
             pitch,roll,heading=90,0,90
-            if ms<1800 and flight.speed_of_sound>0:
-                pitch=90*math.cos(3*ms/1800)
-            else:
-                pitch=0
-            if pitch < 0:
-                pitch = 0
-            vessel.auto_pilot.set_rotation(pitch,heading,roll)
+            if manu_mode==0:
+                vessel.auto_pilot.sas=False
+            if manu_mode==1:
+                if ms<1800 and flight.speed_of_sound>0:
+                    pitch=90*math.cos(3*ms/1800)
+                else:
+                    pitch=0
+                if pitch < 0:
+                    pitch = 0
+                vessel.auto_pilot.set_rotation(pitch,heading,roll)
+            if manu_mode==2:
+                vessel.auto_pilot.sas_mode=conn.space_center.SASMode.prograde
 class AutoMANO(threading.Thread):
     def run(x):
         print "AutoMANO ON"
         vessel=conn.space_center.active_vessel
         flight=vessel.flight(vessel.orbital_reference_frame)
+        s,ps=-1,-1
+        global manu_mode
         while True:
             ms=flight.mach*flight.speed_of_sound
             maxThrottle=1
@@ -41,15 +48,26 @@ class AutoMANO(threading.Thread):
                 maxThrottle=0
             if maxThrottle>1:
                 maxThrottle=1
+
             if vessel.orbit.apoapsis_altitude<targetAltitude:
+                s=1
+                manu_mode=1
                 vessel.control.throttle=maxThrottle
-            elif vessel.orbit.periapsis_altitude < targetAltitude and vessel.orbit.time_to_apoapsis > preApoWait1 and vessel.orbit.time_to_periapsis > vessel.orbit.time_to_apoapsis:
+            elif vessel.orbit.periapsis_altitude < targetAltitude and vessel.orbit.time_to_apoapsis > preApoWait2  and vessel.orbit.time_to_periapsis > vessel.orbit.time_to_apoapsis:
+                s=2
                 vessel.control.throttle=0
-            elif vessel.orbit.periapsis_altitude < targetAltitude and vessel.orbit.time_to_apoapsis < preApoWait2 or vessel.orbit.time_to_periapsis < vessel.orbit.time_to_apoapsis:
+                manu_mode=2
+            elif (vessel.orbit.periapsis_altitude < targetAltitude and vessel.orbit.time_to_apoapsis < preApoWait1) or vessel.orbit.time_to_periapsis < vessel.orbit.time_to_apoapsis:
+                s=3
                 vessel.control.throttle=maxThrottle
-            else:
+                manu_mode=2
+            elif vessel.orbit.periapsis_altitude > targetAltitude:
+                s=4
                 vessel.control.throttle=0
-            pass
+                manu_mode=0
+            if s<>ps:
+                print "MANO FROM",ps,"TO",s
+            ps=s
 
 print "Awaiting connection"
 conn = krpc.connect(name='Kontroller')
@@ -61,6 +79,7 @@ while not mano.isAlive():
     print ".",
 print "OK"
 
+manu_mode=1
 manu=AutoMANU()
 print "Starting manu",
 manu.start()
